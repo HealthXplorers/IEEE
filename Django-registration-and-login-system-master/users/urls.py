80680aea080c4310459610fa53450883b2a42e34
HealthXplorers <147233659+HealthXplorers@users.noreply.github.com>
2023-10-08 13:29:36 +0530

    Delete Django-registration-and-login-system-master directory

--- Django-registration-and-login-system-master/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import home, profile, RegisterView
-
-urlpatterns = [
-    path('', home, name='users-home'),
-    path('register/', RegisterView.as_view(), name='users-register'),
-    path('profile/', profile, name='users-profile'),
-]
